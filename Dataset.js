a2ef9db81802eb3ac72be470a200975e2483077f
Adhi0917 <105773132+Adhi0917@users.noreply.github.com>
2023-01-26 17:22:00 +0530

    Update Dataset.js

--- Dataset.js
+++ Dataset.js
@@ -1,28 +1,57 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import React from "react";
+import './App.css';
 
-export default function Dataset(){
-    const [data, setData] = useState([]);
-    const [columnsDef, setColumnsDef] = useState([]);
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+import {useState, useRef, useEffect, useMemo, useCallback} from 'react';
 
-    useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(res => res.json())
-        .then(data=> {
-            setData(data);
+function Dataset() {
 
-            setColumnsDef(Object.keys(data[0].map(key =>({
-                headerName: key, field : key
-            }))));
-        });
-    },[]);
+  const [rowData, setRowData] = useState();
 
-    return(
-        <div className="ag-theme-alpine" style={{height: '500px', width: '600px'}}>
-            <AgGridReact columnDefs={columnsDef} rowData={data} />
-        </div>
-    );
+  const gridRef = useRef();
+
+  const [columnDefs, setColumnDefs] = useState([
+    {field: 'make'},
+    {field: 'model'},
+    {field: 'price'}
+  ]);
+
+  const defaultColDef = useMemo( ()=> (
+    {
+      sortable: true, 
+      filter: true
+    }
+  ));
+
+  const cellClickedListener = useCallback( event => {
+    console.log('cellClicked', event);
+  }, []);
+
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData))
+  }, []);
+
+  const buttonListener = useCallback( e => {
+    gridRef.current.api.deselectAll();
+  }, []);
+
+  return (
+    <div>
+      <button onClick={buttonListener}>Push Me</button>
+      <div className="ag-theme-alpine" style={{width: 500, height: 500}}>
+        <AgGridReact 
+            ref={gridRef}
+            rowData={rowData} columnDefs={columnDefs}
+            animateRows={true} rowSelection='multiple'
+            onCellClicked={cellClickedListener}
+            defaultColDef={defaultColDef}/>
+      </div>
+    </div>
+  );
 }
+
+default export Dataset;
